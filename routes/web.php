1084fcce5c59338d96b0adefa67cfb6774e5e91e
Joel Vicente <100007903.joan23@fje.edu>
2024-09-21 20:08:37 +0200

    Modelos, Controladores y Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\SongController;
+use App\Http\Controllers\PlaylistController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('artists', AlbumController::class);
+Route::resource('artists', ArtistController::class);
+Route::resource('artists', SongController::class);
+Route::resource('artists', PlaylistController::class);
+
